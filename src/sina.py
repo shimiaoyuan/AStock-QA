cf117cd1deebc5ac2a5bbba80477a695a2d744bb
miaoyuan.shi <jeff.shi@aispeech.com>
2020-04-03 18:57:38 +0800

    xxx

--- src/sina.py
+++ src/sina.py
@@ -99,6 +99,6 @@ class sina():
             line = line.strip()
             line = line.split('\t')
             print(line,count)
-            time.sleep(1)
+            time.sleep(10)
             code = line[1]
             self.insert_today_data(code,day)
